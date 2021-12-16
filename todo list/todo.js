15e68a7bf9a7b6116c2de29c3f79bae8aa322e4e
osamap4026 <55978228+osamap4026@users.noreply.github.com>
2021-12-16 22:32:56 +0530

    added new features

--- todo list/todo.js
+++ todo list/todo.js
@@ -2,11 +2,24 @@ var lis = document.getElementById('list-1');
 var btn = document.getElementById('btn');
 var list = document.getElementById('todolist');
 var work = document.getElementById('work');
+var task = document.getElementById('task');
 
-btn.addEventListener('click', ()=>{
+
+
+btn.addEventListener('click', todo);
+
+work.addEventListener("keyup", function(enter){
+    if(enter.keyCode===13){
+        todo();
+    }
+});
+
+function todo(){
     var no = list.childElementCount + 1;
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(work.value));
     li.id = "item " + no;
     list.insertBefore(li, lis);
-});
+    work.value = '';
+    task.innerHTML = no;
+}
